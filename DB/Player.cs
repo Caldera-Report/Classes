c1a39cb6bec49b98b416efb516814a1f6190ed1a
ChopstickLeg <carpenterhayden1010@gmail.com>
2025-08-09 13:05:21 -0400

    adding displayname code to player class

--- DB/Player.cs
+++ DB/Player.cs
@@ -3,8 +3,9 @@
     public class Player
     {
         public long Id { get; set; }
-        public string DisplayName { get; set; }
         public int MembershipType { get; set; }
+        public string DisplayName { get; set; }
+        public int DisplayNameCode { get; set; }
 
     }
 }
