e7807f46b1ece1d6cf4e0153ed4c26df725e9535
ChopstickLeg <carpenterhayden1010@gmail.com>
2025-08-09 15:10:28 -0400

    Revert "Updating naming"
    This reverts commit 11ceed5dd9f6690b31edc6de1efcb4256deb620b.

---
+++ DB/PlayerActivityRecords.cs
@@ -0,0 +1,9 @@
+﻿public class PlayerActivityRecords
+{
+    public long Id { get; set; }
+    public long PlayerId { get; set; }
+    public DateTime ActivityDate { get; set; }
+    public string FastestCompletion { get; set; }
+    public string TotalTimePlayed { get; set; }
+    public string TotalGamesPlayed { get; set; }
+}
