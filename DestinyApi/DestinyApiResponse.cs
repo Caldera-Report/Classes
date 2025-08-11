f056ba85b4f11077857782f4b7d08adac25d6ff6
Hayden Carpenter <hayden.carpenter@amwins.com>
2025-08-11 15:27:45 -0400

    Yeah, did that last update wrong

--- DestinyApi/DestinyApiResponse.cs
+++ DestinyApi/DestinyApiResponse.cs
@@ -2,7 +2,16 @@
 {
     public class DestinyApiResponse<T>
     {
-        public T Response { get; set; }
+        public required T Response { get; set; }
+        public int ErrorCode { get; set; }
+        public int ThrottleSeconds { get; set; }
+        public required string ErrorStatus { get; set; }
+        public required string Message { get; set; }
+        public required Dictionary<string, string> MessageData { get; set; }
+    }
+
+    public class DestinyApiResponse
+    {
         public int ErrorCode { get; set; }
         public int ThrottleSeconds { get; set; }
         public required string ErrorStatus { get; set; }
