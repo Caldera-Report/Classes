e9a4c82dab736ad24b88a7220cb54844c2f64de5
ChopstickLeg <carpenterhayden1010@gmail.com>
2025-08-13 20:28:39 -0400

    I hate that I have to do this. Adding mapping table to condense duplicate activity records from D2 API

---
+++ DB/ActivityHashMapping.cs
@@ -0,0 +1,9 @@
+﻿namespace Classes.DB
+{
+    public class ActivityHashMapping
+    {
+        public long SourceHash { get; set; }
+        public long CanonicalActivityId { get; set; }
+        public Activity CanonicalActivity { get; set; }
+    }
+}
