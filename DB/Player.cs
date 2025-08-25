6d6c4c8d7d96acbee1e409f8075676528164de3f
ChopstickLeg <carpenterhayden1010@gmail.com>
2025-08-25 19:32:29 -0400

    Adding last time player profile was viewed for calculating priority

--- DB/Player.cs
+++ DB/Player.cs
@@ -10,6 +10,7 @@
         public long? LastPlayedActivityId { get; set; }
         public DateTime LastUpdateStarted { get; set; }
         public DateTime LastUpdateCompleted { get; set; }
+        public DateTime LastProfileView { get; set; }
         public int UpdatePriority { get; set; }
         public string LastUpdateStatus { get; set; }
 
