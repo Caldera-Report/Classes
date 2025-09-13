a6cd27512e716fd1fa3c1578922390082e0166b2
ChopstickLeg <carpenterhayden1010@gmail.com>
2025-09-13 15:21:50 -0400

    Adding some missing activities, and adding index to activities

--- DB/Activity.cs
+++ DB/Activity.cs
@@ -5,6 +5,7 @@
         public long Id { get; set; }
         public string Name { get; set; }
         public string ImageURL { get; set; }
+        public int Index { get; set; }
         public int OpTypeId { get; set; }
 
         public ICollection<ActivityReport> ActivityReports { get; set; }
--- DestinyApi/DestinyApiConstants.cs
+++ DestinyApi/DestinyApiConstants.cs
@@ -18,79 +18,81 @@ namespace Classes.DestinyApi
 
         public static List<uint> SoloOperations = new List<uint>
         {
-            1604785891,
-            1734879670,
-            2489241976,
-            3056702872,
-            3214228047,
-            4008836256,
-            3816084061,
-            2700525707,
-            2619873230,
-            4192328901
+            1604785891, //K1
+            1734879670, //Skywatch
+            2489241976, //Caldera
+            3056702872, //Conflux
+            3214228047, //Creation
+            4008836256, //Salt Mines
+            3816084061, //Quickplay: Normal
+            2700525707, //Quickplay: Advanced
+            2619873230, //Quickplay: Expert
+            4192328901 //Quickplay: Master
         };
 
         public static List<uint> FireteamOperations = new List<uint>
         {
-            47604735  ,
-            112486683 ,
-            1019762304,
-            1136877027,
-            1191481675,
-            1509484286,
-            1655726829,
-            1977716129,
-            2751501766,
-            2963591247,
-            3061857094,
-            3610118907,
-            4145089682,
-            86337131  ,
-            154066551 ,
-            763890651 ,
-            1671362073,
-            1763799972,
-            1826451826,
-            1952657885,
-            2003481787,
-            2396783420,
-            2638370572,
-            3363349123,
-            218823224 ,
-            1459273759,
-            732075595 ,
-            849675207 ,
-            2582324487,
-            2866658313,
-            2694614320,
-            3437865231
+            47604735  , //The Warrior
+            112486683 , //Battleground Delve
+            1019762304, //Technocrat
+            1136877027, //Liminality
+            1191481675, //Battleground Oracle
+            1509484286, //Inverted Spire
+            1655726829, //Battleground Conduit
+            1977716129, //Battleground Foothold
+            2751501766, //Savathun's Spire
+            2963591247, //Dark Priestess
+            3061857094, //Proving Grounds
+            3610118907, //Devil's Lair
+            4145089682, //Glassway
+            86337131  , //Battleground Behemoth
+            154066551 , //Cabal's Watch
+            763890651 , //Hypernet Current
+            1671362073, //Fallen Bunker
+            1763799972, //Battleground Core
+            1826451826, //Expedition Europa
+            1952657885, //Insight Terminus
+            2003481787, //Expedition Cosmodrome
+            2396783420, //Expedition Nessus
+            2638370572, //Conductor's Keep
+            3363349123, //Fallen S.A.B.E.R.
+            218823224 , //The Coil
+            1459273759, //Contest of Elders
+            732075595 , //Onslaught: Widow's Court
+            849675207 , //Onslaught: Midtown
+            2582324487, //Quickplay: Normal
+            2866658313, //Quickplay: Advanced
+            2694614320, //Quickplay: Expert
+            3437865231 //Quickplay: Master
         };
 
         public static List<uint> PinnacleOperations = new List<uint>
         {
-            576782083,
-            1768099736,
-            1948474391,
-            3120544689,
-            3584571989,
-            3996127757,
-            1325548022
+            576782083, //Whisper
+            1768099736, //Starcrossed
+            1948474391, //Kell's Fall
+            3120544689, //Encore
+            775989808, //Avalon
+            935938264, //Presage
+            3584571989, //Nightmare of Gahlran
+            3996127757, //Hefund's Vengance
+            1325548022 //Beneath the Necropolis
         };
 
         public static List<uint> Conquests = new List<uint>
         {
-            3656747069,
-            4089129430,
-            2500578747,
-            2883193556,
-            1645244833,
-            1561490698,
-            2404075359,
-            1025079976,
-            1298573781,
-            2384839795,
-            123652462,
-            3645820853
+            3656747069, //Expert Conquest: Devils' Lair: Customize
+            4089129430, //Expert Conquest: Dark Priestess: Customize
+            2500578747, //Master Conquest: Conduit: Customize
+            2883193556, //Master Conquest: Inverted Spire: Customize
+            1645244833, //Grandmaster Conquest: Whisper: Customize
+            1561490698, //Grandmaster Conquest: Delve: Customize
+            2404075359, //Grandmaster Conquest: Fallen S.A.B.E.R.: Customize
+            1025079976, //Grandmaster Conquest: Glassway: Customize
+            1298573781, //Grandmaster Conquest: Fikrul's Castle: Customize
+            2384839795, //Grandmaster Conquest: Savathûn's Spire: Customize
+            123652462, //Ultimate Conquest: Hypernet: Customize
+            3645820853 //Ultimate Conquest: //node.ovrd.AVALON//: Customize
         };
 
         public static List<OpType> OperationTypes = new List<OpType>
